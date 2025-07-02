fa7dfedf483d7b24f2454cabafc45a73ae1acd8d
Sleepless <vadimbalysev@web.de>
2025-07-02 22:11:44 +0200

    fix pnpm builds

--- next.config.mjs
+++ next.config.mjs
@@ -11,8 +11,8 @@ const nextConfig = {
   },
   experimental: {
     optimizePackageImports: ['lucide-react', '@radix-ui/react-icons'],
-    serverComponentsExternalPackages: ['mysql2']
   },
+  serverExternalPackages: ['mysql2'],
   env: {
     CUSTOM_KEY: process.env.CUSTOM_KEY,
   },
